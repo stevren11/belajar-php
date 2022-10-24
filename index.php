9b5804c21ee3a8cdfd2425c1f5c78f6250e4e7a6
Stevren Christian Emor <stevrentrikster11@gmail.com>
2022-10-24 12:14:39 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+
+echo "Hello World!<br>";
+
+$nama = "Stevren";
+$umur = 18;
+
+echo "Nama saya <strong>$nama</strong>, saya berusia $umur tahun.";
+
+$namaAdik = "Tian";
+$umurAdik = 6;
+
+echo "Nama adik saya <strong>$namaAdik</strong>, adik saya berusia $umurAdik tahun.<br>";
+
+$selisihUmur = $umur - $umurAdik;
+echo "selisih usia saya dan adik adalah $selisihUmur tahun.";
+
+
+?>
