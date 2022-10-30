b12d4cceb878e449edd6d6b6c0339e7dc0bddadb
Stevren Christian Emor <stevrentrikster11@gmail.com>
2022-10-30 23:06:16 +0700

    initial commit

---
+++ php-lanjutan.php
@@ -0,0 +1,28 @@
+<?php
+
+$nama = "Stevren";
+/*
+$nama ="Sevren";
+
+echo $nama;
+echo "<br/>";
+echo $nama;
+echo "<br/>";
+echo $nama;
+echo "<br/>";
+echo $nama;
+echo "<br/>";
+echo $nama;
+echo "<br/>";
+*/
+
+
+$no = 10;
+for ($i=0; $i<$no; $i++) {
+    $n = $i + 1;
+    echo $n." " .$nama."<br/>";
+}
+
+
+
+?>
